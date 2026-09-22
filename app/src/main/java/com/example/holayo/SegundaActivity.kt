1c1eca4723ad5cf812ec1a4b204a8713cbbf87d4
sergiorock <lopez.sergio.n@gmail.com>
2026-09-22 15:45:16 -0300

    sobreescribe oncreate en segunda activity para agregar logica

--- app/src/main/java/com/example/holayo/SegundaActivity.kt
+++ app/src/main/java/com/example/holayo/SegundaActivity.kt
@@ -2,6 +2,7 @@ package com.example.holayo
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +10,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SegundaActivity : AppCompatActivity() {
+    private var contador = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("VIDA", "Segunda → onCreate")
         setContentView(R.layout.activity_segunda)
         val nombre = intent.getStringExtra("nombre") ?: "misterioso visitante"
-        findViewById<TextView>(R.id.tvBienvenida).text = "Sala de experimentos de $nombre"
+        findViewById<TextView>(R.id.tvBienvenida).text =
+            "Sala de experimentos de $nombre"
+        val tvContador = findViewById<TextView>(R.id.tvContador)
+        tvContador.text = contador.toString()
+        findViewById<Button>(R.id.btnSumar).setOnClickListener {
+            contador++
+            tvContador.text = contador.toString()
+        }
     }
     override fun onStart() { super.onStart(); Log.d("VIDA", "Segunda → onStart") }
     override fun onResume() { super.onResume(); Log.d("VIDA", "Segunda → onResume")
