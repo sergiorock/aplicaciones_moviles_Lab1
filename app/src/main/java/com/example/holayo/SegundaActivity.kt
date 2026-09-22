70b3cf34b0817e713c4745ca305af6d43cda4ab1
sergiorock <lopez.sergio.n@gmail.com>
2026-09-22 14:11:47 -0300

    recibe datos en segunda activity

--- app/src/main/java/com/example/holayo/SegundaActivity.kt
+++ app/src/main/java/com/example/holayo/SegundaActivity.kt
@@ -1,6 +1,7 @@
 package com.example.holayo
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_segunda)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val nombre = intent.getStringExtra("nombre") ?: "misterioso visitante"
+        findViewById<TextView>(R.id.tvBienvenida).text = "Sala de experimentos de $nombre"
     }
 }
